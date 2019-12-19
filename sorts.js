2a3f3ffc87735e6b1a526108a1d6aafd3c0005b1
reifnotreef <bridgerrhammond@gmail.com>
2019-12-19 10:51:06 -0600

    funcs in place

--- sorts.js
+++ sorts.js
@@ -1,3 +1,5 @@
+// importing funcs from funcs.js
+const scratch = require("./funcs");
 // #1 Given the following list of numbers 21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
 // 1.1 What is the resulting list that will be sorted after 3 recursive calls to mergesort?
 // 1.2 What is the resulting list that will be sorted after 16 recursive calls to mergesort?
